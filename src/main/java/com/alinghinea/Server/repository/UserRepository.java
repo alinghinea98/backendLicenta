c47ecba48d7d06591133109db51ada48e9b0fe6a
alinghinea98 <52961335+alinghinea98@users.noreply.github.com>
2021-03-13 00:45:26 +0200

    database modif

--- src/main/java/com/alinghinea/Server/repository/UserRepository.java
+++ src/main/java/com/alinghinea/Server/repository/UserRepository.java
@@ -17,20 +17,20 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	
 	boolean existsByUsername(String username);
 	
-	@Query(value = "SELECT EXISTS(SELECT * FROM application_db.utilizatori WHERE application_db.utilizatori.username = :username)", 
+	@Query(value = "SELECT EXISTS(SELECT * FROM 16Ec3VcgYd.utilizatori WHERE 16Ec3VcgYd.utilizatori.username = :username)", 
 			nativeQuery = true)
 	boolean existsByUsernameNotId(@Param("username") String username);
 
 	User getById(Long id);
 	
-	@Query(value = "SELECT * FROM application_db.utilizatori WHERE username = :username AND password = :password", 
+	@Query(value = "SELECT * FROM 16Ec3VcgYd.utilizatori WHERE username = :username AND password = :password", 
 			nativeQuery = true)
 	User getByUsernameAndPassword(@Param("username")String username, @Param("password") String password);
 	
 	long deleteByUsername(String username); // for admin type only
 	
 	@Modifying
-	@Query(value="UPDATE  application_db.utilizatori SET is_deleted = 1 where username = :username AND role = :role", nativeQuery = true)
+	@Query(value="UPDATE  16Ec3VcgYd.utilizatori SET is_deleted = 1 where username = :username AND role = :role", nativeQuery = true)
 	void deleteUserByRole(@Param("username") String username, @Param("role") String role); 
 	
 }
