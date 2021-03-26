bf7def9b3e85bf33274f654f8ec9685c94741e2d
alinghinea98 <52961335+alinghinea98@users.noreply.github.com>
2021-03-26 21:46:43 +0200

    change dto

--- src/main/java/com/alinghinea/Server/dto/KitDto.java
+++ src/main/java/com/alinghinea/Server/dto/KitDto.java
@@ -1,57 +1,67 @@
 package com.alinghinea.Server.dto;
 
-import javax.validation.constraints.Size;
+import com.alinghinea.Server.entities.Sensors;
 
 public class KitDto extends BaseDto {
     private static final long serialVersionUID = 1L;
 
-    @Size(min = 0, max = 255)
-    private String sensorType1;
+    private Sensors sensors;
 
-    @Size(min = 0, max = 255)
-    private String sensorType2;
+    public Sensors getSensors() {
+        return sensors;
+    }
 
-    @Size(min = 0, max = 255)
-    private String sensorType3;
+    public void setSensors(Sensors sensors) {
+        this.sensors = sensors;
+    }
 
-    @Size(min = 0, max = 255)
-    private String sensorType4;
-
+    //    @Size(min = 0, max = 255)
+//    private String sensorType1;
+//
 //    @Size(min = 0, max = 255)
-//    private User user;
-
-
-    public String getSensorType1() {
-        return sensorType1;
-    }
-
-    public void setSensorType1(String sensorType1) {
-        this.sensorType1 = sensorType1;
-    }
-
-    public String getSensorType2() {
-        return sensorType2;
-    }
-
-    public void setSensorType2(String sensorType2) {
-        this.sensorType2 = sensorType2;
-    }
-
-    public String getSensorType3() {
-        return sensorType3;
-    }
-
-    public void setSensorType3(String sensorType3) {
-        this.sensorType3 = sensorType3;
-    }
-
-    public String getSensorType4() {
-        return sensorType4;
-    }
-
-    public void setSensorType4(String sensorType4) {
-        this.sensorType4 = sensorType4;
-    }
+//    private String sensorType2;
+//
+//    @Size(min = 0, max = 255)
+//    private String sensorType3;
+//
+//    @Size(min = 0, max = 255)
+//    private String sensorType4;
+//
+////    @Size(min = 0, max = 255)
+////    private User user;
+//
+//
+//    public String getSensorType1() {
+//        return sensorType1;
+//    }
+//
+//    public void setSensorType1(String sensorType1) {
+//        this.sensorType1 = sensorType1;
+//    }
+//
+//    public String getSensorType2() {
+//        return sensorType2;
+//    }
+//
+//    public void setSensorType2(String sensorType2) {
+//        this.sensorType2 = sensorType2;
+//    }
+//
+//    public String getSensorType3() {
+//        return sensorType3;
+//    }
+//
+//    public void setSensorType3(String sensorType3) {
+//        this.sensorType3 = sensorType3;
+//    }
+//
+//    public String getSensorType4() {
+//        return sensorType4;
+//    }
+//
+//    public void setSensorType4(String sensorType4) {
+//        this.sensorType4 = sensorType4;
+//    }
 
 //    public User getUser() {
 //        return user;
