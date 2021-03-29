5c9759fcf5570929c7e5125f54c2be5368711436
alinghinea98 <52961335+alinghinea98@users.noreply.github.com>
2021-03-29 22:12:51 +0300

    status

--- src/main/java/com/alinghinea/Server/dto/SensorsDto.java
+++ src/main/java/com/alinghinea/Server/dto/SensorsDto.java
@@ -1,6 +1,7 @@
 package com.alinghinea.Server.dto;
 
 
+import com.alinghinea.Server.entities.Kit;
 import com.alinghinea.Server.entities.Sensor;
 
 import java.util.List;
@@ -10,6 +11,16 @@ public class SensorsDto extends BaseDto{
 	private static final long serialVersionUID = 1L;
 
 	List<Sensor> sensorList;
+
+	private Kit kit;
+
+	public Kit getKit() {
+		return kit;
+	}
+
+	public void setKit(Kit kit) {
+		this.kit = kit;
+	}
 
 	public List<Sensor> getSensorList() {
 		return sensorList;
--- src/main/java/com/alinghinea/Server/entities/Sensors.java
+++ src/main/java/com/alinghinea/Server/entities/Sensors.java
@@ -9,40 +9,6 @@ public class Sensors extends BaseEntity {
 	
 	private static final long serialVersionUID = 1L;
 
-//	@Column(name="SENSOR")
-//	@OneToMany(mappedBy = "sensors")
-//	private List<Sensor> sensorList;
-
-//	@OneToOne(fetch = FetchType.LAZY)
-//	@JoinColumn(name="kit_id")
-//	private Sensors sensors;
-
-//	public Sensors(List<Sensor> sensorList, Sensors sensors) {
-//		this.sensorList = sensorList;
-////		this.sensors = sensors;
-//	}
-//
-//	public Sensors() {
-//	}
-
-//	public List<Sensor> getSensorList() {
-//		return sensorList;
-//	}
-//
-//	public void setSensorList(List<Sensor> sensorList) {
-//		this.sensorList = sensorList;
-//	}
-
-
-//	public Sensors getSensors() {
-//		return sensors;
-//	}
-//
-//	public void setSensors(Sensors sensors) {
-//		this.sensors = sensors;
-//	}
-
-
 	@OneToOne(optional = true)
 	@JoinColumn(name = "kit_id")
 	private Kit kit;
