be4903dc656bad0c42e1746bdeb06726c5779078
alinghinea98 <52961335+alinghinea98@users.noreply.github.com>
2021-03-29 15:15:25 +0300

    switch to native query for getting kit ids

--- src/main/java/com/alinghinea/Server/repository/KitRepository.java
+++ src/main/java/com/alinghinea/Server/repository/KitRepository.java
@@ -2,9 +2,13 @@ package com.alinghinea.Server.repository;
 
 import com.alinghinea.Server.entities.Kit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface KitRepository extends JpaRepository<Kit, Long> {
 
 
-    Kit getKitById(Long id);
+    @Query(value = "SELECT * FROM 16Ec3VcgYd.kit WHERE id = :id",
+            nativeQuery = true)
+    Kit getKitById(@Param("id") Long id);
 }
